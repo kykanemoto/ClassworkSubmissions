e8300d6198fa6aab63ba2fec9e1c54f390ba76f0
kykanemoto <146883803+kykanemoto@users.noreply.github.com>
2024-03-19 07:41:46 -1000

    Update CountingCharacters.java

--- CountingCharacters.java
+++ CountingCharacters.java
@@ -15,16 +15,21 @@ public class DiceRoller
  */
 	private static int countChars(String string, char c) 
 	{
+		//initialize count
 		int count = 0;
+		//make a for loop that goes through each character in the string
 		for (int i=0;i< string.length();i++)
 		{
+			//make an if statement if the string at the index is c
 			if(string.charAt(i) == c)
 			{
+				//add one to count to keep track of how many characters are c
 				count ++;
 			}
 		}
 		//print out statement to check
 		 System.out.println(count);
+		//if the statement is false return count
 		 return count;
 	}
 }
