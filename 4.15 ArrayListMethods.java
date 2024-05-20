1bc2826846f807dbd412492c9dee85da0c5deef9
kykanemoto <146883803+kykanemoto@users.noreply.github.com>
2024-05-20 00:10:59 -1000

    Update 4.15 ArrayListMethods.java

--- 4.15 ArrayListMethods.java
+++ 4.15 ArrayListMethods.java
@@ -13,32 +13,34 @@ public class ArrayListMethodInDepth {
 		foods.add("Apples");
 		foods.add("Bread");
 		foods.add("Cookies");
+		
 		//Task 1: Print out the length of the foods ArrayList.
-		//Method will print the ArrayList length. In this case, it take the ArrayList and return 3.
+		//.size() method will get the length of an ArrayList. 
+		//In this case, it will take the ArrayList and return the length which is 3.
 		System.out.println(foods.size());
 		
 		//Task 2: Inside of a print statement, add the String "Dole Whip" to foods. 
-		//In the line comment for this code, state what it prints. 
 		//It will print true.
-		//Method will add another String to the end of the ArrayList. In this case, it will add Dole Whip to the
-		//end of the ArrayList and return true.
+		//.add(E) method will add another element to the end of the ArrayList (here it will be Dole Whip)
+		//It will add Dole Whip to the end of the ArrayList and return a boolean (here it will be true)
 		System.out.println(foods.add("Dole Whip"));
 		System.out.println(foods.size());
 
-		//Task 3: Inside of a print statement, use the .set(E) method to change "Bread" in foods to "Butter". Comment what it prints.
+		//Task 3: Inside of a print statement, use the .set(E) method to change "Bread" in foods to "Butter".
 		//It will print Bread
-		//Method will change the string at that index to the new string. In this case, it will change Bread to Butter and return Bread.
+		//.set(int, E) method replace the element of an ArrayList at the specific index (here it is Bread at index 1)
+		//and replaces it with a new element (here it is Butter)
+		//In this case, it will change Bread to Butter and return Bread.
 		System.out.println(foods.set(1, "Butter"));
 		System.out.println(foods.size());
 
-		//Task 4: Inside of a print statement, use the .remove(int) method to remove "Cookies" from foods. Comment what it prints.
-		//It will print true
-		//Method will remove the String from the ArrayList. In this case, it will remove Cookie from the ArrayList and return true.
-		System.out.println(foods.remove("Cookies"));
+		//Task 4: Inside of a print statement, use the .remove(int) method to remove "Cookies" from foods.
+		//It will print Cookies
+		//.remove() method removes an element from an ArrayList at the specific index (here it is Cookies at index 2)
+		//and will return the element that was removed (here it is Cookies)
+		//In this case, it will remove Cookie from the ArrayList and return Cookies.
+		System.out.println(foods.remove(2));
 		System.out.println(foods.size());
-		
-		//Task 5: For each task, go back and comment what the method you are calling does, 
-		//and what the method returns (both in general and in this case).
 		
 
 	}
